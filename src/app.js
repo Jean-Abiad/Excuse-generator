e71eff3e43114d36ea081dd3d34a9d3ebf560b09
Jean-Abiad <116680148+Jean-Abiad@users.noreply.github.com>
2024-01-18 18:10:01 -0400

    Update app.js

--- src/app.js
+++ src/app.js
@@ -7,5 +7,24 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+    let theExcuse = () => {
+    let who = [
+      "mi padre", "la tortuga", "el perro", "mi vecino", "Samuel", "Bolivar",];
+    let action = [
+      "comio", "espropio", "robo", "destrozo", "rompio", "perdio","escondio", "bataqueo", "robo" , "se espropio"];
+    let what = [
+      "mi tarea", "la comida", "mi celular", "las llaves", "el carro", "la nave", "el generador de excusas",];
+    let when = [
+      "despues de clases", "Mientras cagaba", "cuando estaba hablando", "cuando dormia", "cuando me fui", "durante el almuerzo", "mientras jugaba",];
+
+    let quien = Math.floor(Math.random() * who.length);
+    let accion = Math.floor(Math.random() * action.length);
+    let que = Math.floor(Math.random() * what.length);
+    let cuando = Math.floor(Math.random() * when.length); 
+
+    return who[quien] + " " + action[accion] + " " + what[que] + " " + when[cuando]
+
+  }
+  let excusa = theExcuse();
+  console.log(excusa);
 };
